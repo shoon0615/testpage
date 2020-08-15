0a636cd12e994e381e103b74ff0d1865a52b58e9
이상훈 <@DESKTOP-JE3PPQE>
2020-08-15 22:58:16 +0900

    delete HomeController

--- src/main/java/com/exe/test/HomeController2.java
+++ src/main/java/com/exe/test/HomeController2.java
@@ -15,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class HomeController2 {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController2.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
